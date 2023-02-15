c2e7028d8f35063f6b91f0249551ef098b3aa821
Satisanya <novoseltsev7g21@gmail.com>
2023-02-15 17:06:04 +0300

    Добавление pad и theme
    # Conflicts:
    #	src/app/EducationPage/education-info-tile/education-info-tile.component.scss

--- src/app/EducationPage/data-service.ts
+++ src/app/EducationPage/data-service.ts
@@ -1,17 +1,19 @@
 import { InfoTileItem } from "./education-info-tile/education-info-tile-item";
-import {} from '@angular/core';
+import { } from '@angular/core';
 
 export class DataService {
     private tileList: InfoTileItem[] = [
-        {title: "", tooltip: "", imagePath: "https://cdn-icons-png.flaticon.com/512/342/342757.png"},
-        {title: "", tooltip: "", imagePath: "https://cdn-icons-png.flaticon.com/512/342/342757.png"}
+        { title: "", tooltip: "", imagePath: "https://cdn-icons-png.flaticon.com/512/342/342757.png" },
+        { title: "", tooltip: "", imagePath: "https://cdn-icons-png.flaticon.com/512/342/342757.png" },
+        { title: "", tooltip: "", imagePath: "https://cdn-icons-png.flaticon.com/512/342/342757.png" },
     ];
+
     private infoText: string = "";
 
-    constructor(){
+    constructor() {
     }
 
-    public get infoTileList(): InfoTileItem[]{
+    public get infoTileList(): InfoTileItem[] {
         return this.tileList;
     }
 
