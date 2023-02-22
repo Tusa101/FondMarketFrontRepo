687ad32468f7ed2636076a064abd9d53194a16fa
Satisanya <novoseltsev7g21@gmail.com>
2023-02-22 18:38:27 +0300

    Добавлен Wiki, Открорректированы размеры и параметры отображения плиток

--- src/app/EducationPage/data-service.ts
+++ src/app/EducationPage/data-service.ts
@@ -3,8 +3,8 @@ import {} from '@angular/core';
 
 export class DataService {
     private tileList: InfoTileItem[] = [
-        {title: "", tooltip: "", imagePath: "https://cdn-icons-png.flaticon.com/512/342/342757.png"},
-        {title: "", tooltip: "", imagePath: "https://cdn-icons-png.flaticon.com/512/342/342757.png"}
+        {title: "", tooltip: "", imagePath: "https://sun9-19.userapi.com/impg/FK4IZ3KUV9wZZIhKGgD3U6rjDuOyt4WtRUORLw/b6o4kgurx6s.jpg?size=251x251&quality=96&sign=1c5f41d24433e11d9f57677686b95057&type=album"},
+        {title: "", tooltip: "", imagePath: "https://sun9-19.userapi.com/impg/FK4IZ3KUV9wZZIhKGgD3U6rjDuOyt4WtRUORLw/b6o4kgurx6s.jpg?size=251x251&quality=96&sign=1c5f41d24433e11d9f57677686b95057&type=album"}
     ];
     private infoText: string = "";
 
